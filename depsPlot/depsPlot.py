0d08e407928301580fbed780b3b22d72222678ad
Pawel Kowalczuk <pwl.kowalczuk@gmail.com>
2019-12-18 19:30:43 +0100

    building parameter project with error cause to tired today

--- depsPlot/depsPlot.py
+++ depsPlot/depsPlot.py
@@ -3,10 +3,24 @@ from .debug.log import logger
 from .debug.log import setup_log_level
 from .debug.log import DEBUG
 
+import subprocess
+
 
 @click.command()
 @click.option('--verbose', is_flag=True, help='will do a verbose mode')
-@click.option('--xcodeproj', required=True, help='')
-def cli(verbose):
+@click.option('--proj', required=True, help='')
+def cli(verbose, proj):
     setup_log_level(level=DEBUG)
-    logger.debug("some example log")
+    logger.debug('some example log')
+    build_project(name=proj)
+
+
+def build_project(name: str):
+    logger.debug(f'BUILDING PROJECT {name}')
+    result = subprocess.check_output(['xcodebuild', 
+                                      '-showBuildSettings',
+                                      name,
+                                      'build',
+                                      'CODE_SIGNING_ALLOWED=NO',
+                                      'CODE_SIGNING_REQUIRED=NO'])
+    logger.debug(result)
